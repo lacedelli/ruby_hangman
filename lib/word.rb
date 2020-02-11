dfb4d4cc66a3986a71966001153549c29e314d47
Lacedelli <lacedelli@outlook.com>
2020-02-11 14:08:29 -0600

    added pseudocode for word.rb, which will contain the selected word and the guesses array

---
+++ lib/word.rb
@@ -0,0 +1,36 @@
+# Load word database
+
+# Class start
+	# Define attr readers word, guesses, hidden_word
+
+	# initialize values
+	# word = ""
+	# guesses = [] 
+	# hidden_word = []
+
+	# Def select word
+		# Open word database file
+		# select word of desired length
+		# Assign selected word to local word var
+
+	# def guessed_already?(guess)
+		# compare if guess is already in private array of letters guesses
+		# if true return true
+		# else return false
+
+	# def make_guess(guess)
+		# add guess to guesses array
+		# compare guess with private variable word
+		# if guess is in word 
+			# add guess to hidden word in correct index
+			# return flag for good guess
+		# if guess is not in word
+			# return flag for bad guess
+
+	# def to_s()
+		# return a hash with the selected word
+		# the guesses
+		# and the final state of the hidden word
+
+	# Private
+	# Define attr writers word, guesses, hidden_word

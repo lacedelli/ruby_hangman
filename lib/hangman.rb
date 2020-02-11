9d198684bd0399197157ed94a106cbd39d5db0dd
Lacedelli <lacedelli@outlook.com>
2020-02-11 13:26:27 -0600

    add pseudocode for hangman.rb, which contains a class that outputs a drawing of the titular hanging man

---
+++ lib/hangman.rb
@@ -0,0 +1,19 @@
+# Create class
+# Initialize local variables
+# mistakes = 0
+
+# Variable for hangman with no mistakes
+# Variable for hangman with 1 mistake
+# Variable for hangman with 2 mistakes
+# Variable for hangman with 3 mistakes
+# Variable for hangman with 4 mistakes
+# Variable for hangman with 5 mistakes
+# Variable for hangman with 6 mistakes
+
+# define setter for mistakes variable
+
+# Override to_s
+# returns a different variable for the hangman depending on the value
+# of mistakes variable
+
+# define private accessor for mistakes variable

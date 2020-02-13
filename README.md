6bdde06e933662c0afdd8e54841a41a7e9a34760
Lacedelli <lacedelli@outlook.com>
2020-02-13 13:51:54 -0600

    add the string representations of the hangman and a method that returns them depending on the number of mistakes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ruby_hangman
-Repository for The Odin Project's hangman assignment that can be found at: https://www.theodinproject.com/courses/ruby-programming/lessons/file-i-o-and-serialization?ref=lnav
+Repository for The Odin Project's hangman assignment that can be found at: https://www.theodinproject.com/courses/ruby-programming/lessons/file-i-o-and-serialization
 
 ## Thoughts before dropping in
 
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,22 +1,88 @@
 # Create class
-# define reader for mistakes variable
-# Initialize local variables(mistakes = 0)
-# @mistakes = mistakes
+class Hangman
+	attr_reader :mistakes
 
-# Variable for hangman with no mistakes
-# Variable for hangman with 1 mistake
-# Variable for hangman with 2 mistakes
-# Variable for hangman with 3 mistakes
-# Variable for hangman with 4 mistakes
-# Variable for hangman with 5 mistakes
-# Variable for hangman with 6 mistakes
+	def initialize(mistakes = 0)
+		@mistakes = mistakes
+		
+		@zero_mistakes = "
+		   |---|
+		       |
+		       |
+		       |
+		_______|"
+
+		@one_mistake = "
+		   |---|
+		   O   |
+		       |
+		       |
+		_______|"
+
+		@two_mistakes = "
+		   |---|
+		   O   |
+		   |   |
+		       |
+		_______|"
+
+		@three_mistakes = "
+		   |---|
+		   O   |
+		  /|   |
+		       |
+		_______|"
+
+		@four_mistakes = "
+		   |---|
+		   O   |
+		  /|\\  |
+		       |
+		_______|"
+		
+		@five_mistakes = "
+			 |---|
+		   O   |
+		  /|\\  |
+		  /    |
+		_______|"
+		
+		@six_mistakes = "
+		   |---|
+		   O   |
+		  /|\\  |
+		  / \\  |
+		_______|"
+		
+   
+		end
 
 # define setter for mistakes variable
 
 # define display_status
+	def draw_hangman(number)
+
+		case number
+		when 0
+			@zero_mistakes
+		when 1
+			@one_mistake
+		when 2
+			@two_mistakes
+		when 3
+			@three_mistakes
+		when 4
+			@four_mistakes
+		when 5
+			@five_mistakes
+		when 6
+			@six_mistakes
+		end
+	end
 	# returns a different string depending on the number of mistakes
 
 # Override to_s
 	# Return mistakes as a string value
 
 # define private writer for mistakes variable
+end

94f0482d80a11b3f1a8d3a74b2f0dec20f5e893d
Lacedelli <lacedelli@outlook.com>
2020-02-22 17:45:36 -0600

    add untested first draft of code for game file

--- lib/game.rb
+++ lib/game.rb
@@ -1,32 +1,63 @@
 # Import all necessary local libraries
 # Import json library
 
-# Define saving method
-	# get Hangman status from hangman.to_s() 
-	# get Word status from word.to_hash()
-	# override save.json file with gathered data
-
-# Define method for playing a match of the game
-		# Initiate match loop
-		# Draw hangman
-		# Draw word
-		# Draw previously selected letter
-		# Ask player for a new letter
-		# If input == save
-			# run saving method
-			# puts a farewell message
-			# end the app
-		# Initiate loop if letter is already in guesses
-			# Compare letter with word
-			# If letter is in word, 
-				# add it to word. Break
-			# Else if letter is not in word 
-				# add one to mistakes and update hangman. Break
-		# Check if all the spaces in guessed word have been filled
-		# If true break loop
-
 # define load method
 	# get values and load pump them into respective classes
+
+# TODO define deletor method
+
+# Define saving method
+def save_game(hangman, word)
+	hangman_number = hangman.to_s()
+	word_hash = word.to_hash()
+	# override save.json file with gathered data
+	puts "Successfully saved data!"
+end
+
+# Define method for playing a match of the game
+def game_match(hangman, word)
+	loop do
+		puts hangman.draw_hangman()
+
+		puts "Your progress in guessing the word is:"
+		puts word.hidden_word.join("")
+
+		puts "You have guessed the following letters:"
+		puts word.guesses.join(", ")
+	
+		puts "Please make a new guess:" 
+		puts "Or, alternatively, write \"save\" to save the game's current state."
+
+		loop do 
+		guess = gets().chomp()
+			if guess == "save"
+				save_game(hangman, word)
+				puts "Thanks for playing! See you when you return!"
+				# TODO find out how to exit the program
+			end
+		break if word.valid_guess?(guess) and !word.guessed_already?(guess)
+		end
+
+		result = word.make_guess(guess)
+		if result
+			break
+		else
+			hangman.increment_mistakes()
+			break
+		end
+
+		
+		if word.word_guessed?()
+			# TODO get code from file deletor method
+			break
+		end
+		
+		if word.max_mistakes?
+			# TODO get code from file deletor method
+			break
+		end
+	end
+end
 
 # Initiate game loop
 	# If save exists
@@ -36,6 +67,5 @@
 
 	# Create a new word instance
 	# Call round method
-	# nuke saved file
 	# Ask player if they want to play a new round
 	# If answer == No Break
